f064ec8f726ca1c5e6862b584bde89b610b93bf4
Aden de Leeuw <90319180+adendl@users.noreply.github.com>
2025-07-12 22:21:13 +1000

    project initialisation

---
+++ src/main/java/com/adendl/integration_patterns/IntegrationPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.adendl.integration_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adendl/integration_patterns/IntegrationPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adendl.integration_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
